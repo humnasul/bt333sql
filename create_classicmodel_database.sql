e3a14c907b46d14cbaba1c61e4cb3c3d2ba9596e
Humna <humnasultan@gmail.com>
2024-03-21 10:00:29 -0400

    adding in new query

---
+++ create_classicmodel_database.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Schema ClassicModels
+-- -----------------------------------------------------
+DROP DATABASE IF EXISTS ClassicModels;
+CREATE DATABASE  `ClassicModels` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_cs ;
