e53de309845b70e1da9bb16062bd28bcb5755aa6
Humna <humnasultan@gmail.com>
2024-04-15 19:35:06 -0400

    adding code

---
+++ Select_statements.sql
@@ -0,0 +1,20 @@
+SELECT * FROM classicmodels.offices;
+
+SELECT * FROM classicmodels.offices where city = "Boston";
+
+SELECT * FROM classicmodels.offices where country = "USA";
+
+SELECT * FROM classicmodels.employees;
+
+SELECT * FROM classicmodels.employees where lastname = "Patterson";
+
+SELECT * FROM classicmodels.employees where email = "ykato@classicmodelcars.com";
+
+SELECT * FROM employees JOIN offices
+	ON employees.officeCode = offices.officeCode;
+    
+SELECT offices.city, employees.lastName, employees.firstName, employees.jobTitle
+	AS stkvalue
+	FROM employees JOIN offices
+		ON employees.officeCode = offices.officeCode
+			ORDER BY offices.city, employees.lastName;
